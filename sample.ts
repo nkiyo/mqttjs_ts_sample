08fd3753b6aff8eff170925fdef9d2d393f672d2
nkiyo <nakahara.kiyoshi@gmail.com>
2023-05-14 01:39:42 +0900

    Typescript化

--- sample.ts
+++ sample.ts
@@ -1,16 +1,17 @@
-const mqtt = require('mqtt')
-const client  = mqtt.connect('mqtt://localhost')
+import * as mqtt from "mqtt"
+import { MqttClient } from 'mqtt/types/lib/client'
+
+const client: MqttClient  = mqtt.connect('mqtt://localhost')
 
 client.on('connect', function () {
-  client.subscribe('presence', function (err) {
+  client.subscribe('presence', function (err: Error) {
     if (!err) {
       client.publish('presence', 'Hello mqtt')
     }
   })
 })
 
-client.on('message', function (topic, message) {
-  // message is Buffer
+client.on('message', function (topic: string, message: Buffer) {
   console.log(message.toString())
   client.end()
 })
